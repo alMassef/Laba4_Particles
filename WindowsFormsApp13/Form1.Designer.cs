ea11863f977da1f4087a75b163cc7e74abb575b9
AleksandrCF <aleksandrabz792@gmail.com>
2020-05-12 17:02:23 +0800

    Добавьте файлы проекта.

---
+++ WindowsFormsApp13/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace WindowsFormsApp13
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.picDisplay = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.tbDirection = new System.Windows.Forms.TrackBar();
+            this.tbSpread = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbSpeed = new System.Windows.Forms.TrackBar();
+            this.tdLife = new System.Windows.Forms.TrackBar();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.picDisplay)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tbDirection)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tbSpread)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tbSpeed)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tdLife)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picDisplay
+            // 
+            this.picDisplay.Location = new System.Drawing.Point(12, 63);
+            this.picDisplay.Name = "picDisplay";
+            this.picDisplay.Size = new System.Drawing.Size(804, 416);
+            this.picDisplay.TabIndex = 0;
+            this.picDisplay.TabStop = false;
+            this.picDisplay.MouseMove += new System.Windows.Forms.MouseEventHandler(this.picDisplay_MouseMove);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 40;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // tbDirection
+            // 
+            this.tbDirection.Location = new System.Drawing.Point(12, 12);
+            this.tbDirection.Maximum = -75;
+            this.tbDirection.Minimum = -105;
+            this.tbDirection.Name = "tbDirection";
+            this.tbDirection.Size = new System.Drawing.Size(113, 45);
+            this.tbDirection.TabIndex = 1;
+            this.tbDirection.TickFrequency = 10;
+            this.tbDirection.Value = -75;
+            this.tbDirection.Scroll += new System.EventHandler(this.tbDirection_Scroll);
+            // 
+            // tbSpread
+            // 
+            this.tbSpread.Location = new System.Drawing.Point(131, 12);
+            this.tbSpread.Maximum = 30;
+            this.tbSpread.Name = "tbSpread";
+            this.tbSpread.Size = new System.Drawing.Size(107, 45);
+            this.tbSpread.TabIndex = 2;
+            this.tbSpread.TickFrequency = 5;
+            this.tbSpread.Scroll += new System.EventHandler(this.tbSpread_Scroll);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Направление";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(131, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Разброс";
+            // 
+            // tbSpeed
+            // 
+            this.tbSpeed.Location = new System.Drawing.Point(244, 12);
+            this.tbSpeed.Name = "tbSpeed";
+            this.tbSpeed.Size = new System.Drawing.Size(108, 45);
+            this.tbSpeed.TabIndex = 5;
+            this.tbSpeed.TickFrequency = 2;
+            this.tbSpeed.Scroll += new System.EventHandler(this.tbSpeed_Scroll);
+            // 
+            // tdLife
+            // 
+            this.tdLife.Location = new System.Drawing.Point(358, 12);
+            this.tdLife.Maximum = 50;
+            this.tdLife.Name = "tdLife";
+            this.tdLife.Size = new System.Drawing.Size(116, 45);
+            this.tdLife.TabIndex = 6;
+            this.tdLife.TickFrequency = 10;
+            this.tdLife.Scroll += new System.EventHandler(this.tdLife_Scroll);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(244, 48);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Скорость";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(358, 48);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(132, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Продолжительность пад";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(981, 533);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tdLife);
+            this.Controls.Add(this.tbSpeed);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbSpread);
+            this.Controls.Add(this.tbDirection);
+            this.Controls.Add(this.picDisplay);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.picDisplay)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tbDirection)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tbSpread)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tbSpeed)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tdLife)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picDisplay;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TrackBar tbDirection;
+        private System.Windows.Forms.TrackBar tbSpread;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TrackBar tbSpeed;
+        private System.Windows.Forms.TrackBar tdLife;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ WindowsFormsApp13/Form1.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp13
+{
+    public partial class Form1 : Form
+    {
+        List<DirectionColorfulEmiter> emiters = new List<DirectionColorfulEmiter>();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // привязал изображение
+            picDisplay.Image = new Bitmap(picDisplay.Width, picDisplay.Height);
+
+            var rnd = new Random();
+            for (var i = 0; i < 50; ++i)
+            {
+                emiters.Add(new DirectionColorfulEmiter
+                {
+                    ParticlesCount = 50,
+                    Position = new Point(rnd.Next(picDisplay.Width), -20),
+                    Radius = 2 + rnd.Next(5),
+                });
+            }
+        }
+
+        // добавил функцию обновления состояния системы
+        private void UpdateState()
+        {
+            foreach (var emiter in emiters)
+            {
+                emiter.UpdateState();
+            }
+
+            //foreach (var particle in particles)
+            //{
+            //    particle.Life -= 1; // уменшаю здоровье
+            //                        // если здоровье кончилось
+            //    if (particle.Life < 0)
+            //    {
+            //        // восстанавливаю здоровье
+            //        particle.Life = 20 + Particle.rand.Next(100);
+            //        particle.Speed = 1 + Particle.rand.Next(10);
+
+            //        // генерировать вдоль верхней границы изображения
+            //        particle.X = Particle.rand.Next(picDisplay.Image.Width);
+            //        particle.Y = 0;
+
+            //        // добавил направление движения -90 градусов +-15
+            //        particle.Direction = -90 + 15 - Particle.rand.Next(30);
+            //        particle.Radius = 2 + Particle.rand.Next(10);
+            //    }
+            //    else
+            //    {
+            //        // а это наш старый код
+            //        var directionInRadians = particle.Direction / 180 * Math.PI;
+            //        particle.X += (float)(particle.Speed * Math.Cos(directionInRadians));
+            //        particle.Y -= (float)(particle.Speed * Math.Sin(directionInRadians));
+            //    }
+            //}
+
+            //// добавил генерацию частиц
+            //// генерирую не более 10 штук за тик
+            //for (var i = 0; i < 10; ++i)
+            //{
+            //    if (particles.Count < 500) // пока частиц менье 500 генерируем новые
+            //    {
+            //        // снег белый, поэтому придется использовать ParticleColorful
+            //        var particle = ParticleColorful.Generate();
+
+
+            //        // цвет от белого
+            //        particle.FromColor = Color.White;
+            //        // до белого прозрачного
+            //        particle.ToColor = Color.FromArgb(0, Color.White);
+
+
+            //        // координата X вдоль всей верхней границы может оказаться
+            //        particle.X = Particle.rand.Next(picDisplay.Image.Width);
+            //        particle.Y = 0;
+            //        // направление движения чтобы вниз
+            //        particle.Direction = -90 + 15 - Particle.rand.Next(30);
+
+            //        particles.Add(particle);
+            //    }
+            //    else
+            //    {
+            //        break; // а если частиц уже 500 штук, то ничего не генерирую
+            //    }
+            //}
+        }
+
+        // функция рендеринга
+        private void Render(Graphics g)
+        {
+            foreach (var emiter in emiters)
+            {
+                emiter.Render(g);
+            }
+
+            // утащили сюда отрисовку частиц
+            //foreach (var particle in particles)
+            //{
+            //    particle.Draw(g);
+            //}
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            UpdateState(); // каждый тик обновляем систему
+
+            using (var g = Graphics.FromImage(picDisplay.Image))
+            {
+                g.Clear(Color.Black); // добавил очистку
+
+                Render(g); // рендерим систему
+            }
+
+            // обновить picDisplay
+            picDisplay.Invalidate();
+        }
+
+        private void picDisplay_MouseMove(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void tbDirection_Scroll(object sender, EventArgs e)
+        {
+            foreach (var emiter in emiters)
+            {
+                emiter.Direction = tbDirection.Value;
+            }
+        }
+
+        private void tbSpread_Scroll(object sender, EventArgs e)
+        {
+            foreach (var emiter in emiters)
+            {
+                emiter.Spread = tbSpread.Value;
+            }
+        }
+
+        private void tbSpeed_Scroll(object sender, EventArgs e)
+        {
+            foreach (var emiter in emiters)
+            {
+                emiter.Speed = tbSpeed.Value;
+            }  
+        }
+
+        private void tdLife_Scroll(object sender, EventArgs e)
+        {
+            foreach (var emiter in emiters)
+            {
+                emiter.Life = tdLife.Value;
+            }
+        }
+    }
+}
